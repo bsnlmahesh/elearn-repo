6e1a732804449d5e698a5aaa47e23fa8af9c192e
bsnlmahesh <mahesh.23joy@gmail.com>
2025-04-07 12:14:27 +0530

    rg created

---
+++ rg.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.26.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    features {}
+    subscription_id = "db109632-b912-4b9a-9fe5-bb22e39daf52"
+    # Configuration options
+}
+  # Configuration options
+
+  
